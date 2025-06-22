c72d425760f58eb8e6c5ef5ae214dd95235c5db8
hiy0ung <hyejee98@gmail.com>
2025-06-22 15:49:49 +0900

    chore: db 테이블명 수정 반영

--- src/main/java/org/koreait/planitkorea/repository/ProductRepository.java
+++ src/main/java/org/koreait/planitkorea/repository/ProductRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // 전체 조회
     @Query(value = """
-      WITH Available_Rooms AS (
+      WITH available_rooms AS (
            SELECT
                p.id AS product_id,
                p.product_category,
@@ -26,15 +26,15 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                 WHERE pi.product_id = p.id
                 ORDER BY pi.id ASC
                 LIMIT 1) AS product_image
-           FROM Products p
-           INNER JOIN Product_Cities pc ON pc.product_id = p.id
-           INNER JOIN Cities c ON c.id = pc.city_id
-           INNER JOIN Sub_Products sp ON sp.main_product_id = p.id
+           FROM products p
+           INNER JOIN product_cities pc ON pc.product_id = p.id
+           INNER JOIN cities c ON c.id = pc.city_id
+           INNER JOIN sub_products sp ON sp.main_product_id = p.id
            WHERE c.city_name = :cityName
            AND sp.sub_person >= :person
            AND NOT EXISTS (
                SELECT 1
-               FROM Reservations r
+               FROM reservations r
                WHERE r.sub_product_id = sp.id
                AND r.start_date <= :endDate
                AND r.end_date >= :startDate
@@ -49,9 +49,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
            ar.product_description,
            ar.product_image,
            GROUP_CONCAT(f.id) AS facilityIds
-        FROM Available_Rooms ar
-        LEFT JOIN Product_Facilities pf ON pf.product_id = ar.product_id
-        LEFT JOIN Facilities f ON f.id = pf.facility_id
+        FROM available_rooms ar
+        LEFT JOIN product_facilities pf ON pf.product_id = ar.product_id
+        LEFT JOIN facilities f ON f.id = pf.facility_id
         GROUP BY 
             ar.product_id, 
             ar.product_category, 
@@ -67,7 +67,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                                                 @Param("endDate") LocalDate endDate);
     // 상세 조회
     @Query(value = """
-        WITH Available_Rooms AS (
+        WITH available_rooms AS (
             SELECT
                 p.id AS product_id,
                 p.product_category,
@@ -81,9 +81,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                 sp.sub_price,
                 sp.sub_person,
                 spi.sub_product_image
-            FROM Products p
-            LEFT JOIN Product_images pi ON pi.product_id = p.id
-            INNER JOIN Sub_Products sp ON sp.main_product_id = p.id
+            FROM products p
+            LEFT JOIN product_images pi ON pi.product_id = p.id
+            INNER JOIN sub_products sp ON sp.main_product_id = p.id
             INNER JOIN sub_product_images spi ON spi.sub_product_id = sp.id
             WHERE p.id = :productId
         )
@@ -102,9 +102,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             ar.sub_product_image,
             f.id as facility_id,
             f.facility_name
-        FROM Available_Rooms ar
-        LEFT JOIN Product_Facilities pf ON pf.product_id = ar.product_id
-        LEFT JOIN Facilities f ON f.id = pf.facility_id;
+        FROM available_rooms ar
+        LEFT JOIN product_facilities pf ON pf.product_id = ar.product_id
+        LEFT JOIN facilities f ON f.id = pf.facility_id;
 """, nativeQuery = true)
     List<Object[]> findProductDetailById(@Param("productId") Long productId);
 
@@ -127,10 +127,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             p.product_address,
             p.product_price,
             COUNT(wl.product_id) AS wishlist_count
-        FROM Products p
-        LEFT JOIN Wish_List wl ON p.id = wl.product_id
-        JOIN Product_Cities pc ON pc.product_id = p.id
-        JOIN Cities c ON pc.city_id = c.id
+        FROM products p
+        LEFT JOIN wish_list wl ON p.id = wl.product_id
+        JOIN product_cities pc ON pc.product_id = p.id
+        JOIN cities c ON pc.city_id = c.id
         GROUP BY p.id, p.product_name, p.product_category, c.city_name, p.product_address, p.product_price
         HAVING COUNT(wl.product_id) > 0
         ORDER BY wishlist_count DESC
@@ -138,36 +138,14 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     ) AS p
     LEFT JOIN (
         SELECT pi.product_id, pi.product_image AS image
-        FROM Product_Images pi
+        FROM product_images pi
         INNER JOIN (
             SELECT product_id, MIN(id) AS min_id
-            FROM Product_Images
+            FROM product_images
             GROUP BY product_id
         ) AS first_images ON pi.id = first_images.min_id
     ) AS pi ON p.product_id = pi.product_id
 """, nativeQuery = true)
     List<Object[]> findTop5Products();
-
-//    @Query(value = """
-//    SELECT
-//        p.id AS product_id,
-//        p.product_name AS product_name,
-//        p.product_category AS accommodation_type,
-//        c.city_name AS accommodation_region,
-//        p.product_address AS accommodation_address,
-//        p.product_price AS accommodation_price,
-//        pi.product_image AS accommodation_image,
-//        COUNT(wl.product_id) AS wishlist_count
-//    FROM Products p
-//    LEFT JOIN Wish_List wl ON p.id = wl.product_id
-//    JOIN Product_Cities pc ON pc.product_id = p.id
-//    JOIN cities c ON pc.city_id = c.id
-//    LEFT JOIN Product_Images pi ON pi.product_id = p.id
-//    GROUP BY p.id, p.product_name, p.product_category, c.city_name, p.product_address, p.product_price, pi.product_image
-//    HAVING COUNT(wl.product_id) > 0
-//    ORDER BY wishlist_count DESC
-//    LIMIT 5;
-//
-//""", nativeQuery = true)
-//    List<Object[]> findTop5Products();
 }
+
--- src/main/java/org/koreait/planitkorea/repository/WishListRepository.java
+++ src/main/java/org/koreait/planitkorea/repository/WishListRepository.java
@@ -15,7 +15,7 @@ public interface WishListRepository extends JpaRepository<WishList, Long> {
     Optional<WishList> findByUserIdAndId(Long userId, Long id);
 
     @Query(value = """
-        select
+        SELECT
             w.id as wish_list_id,
             w.user_id,
             w.product_id,
@@ -23,15 +23,15 @@ public interface WishListRepository extends JpaRepository<WishList, Long> {
             p.product_address,
             p.product_price,
             (
-                select pi.product_image
-                from product_images pi
-                where pi.product_id = p.id
-                order by pi.id asc
-                limit 1
+                SELECT pi.product_image
+                FROM product_images pi
+                WHERE pi.product_id = p.id
+                ORDER BY pi.id asc
+                LIMIT 1
             ) as product_image
-        from wish_list w
-        left join products p on p.id = w.product_id
-        where w.user_id = :userId
+        FROM wish_list w
+        LEFT JOIN products p on p.id = w.product_id
+        WHERE w.user_id = :userId
 """, nativeQuery = true)
     List<Object[]> getWishListByUserId(@Param("userId") Long userId);
 }
